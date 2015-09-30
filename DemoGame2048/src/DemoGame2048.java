b8f7a9b072fada1379587b4661c105dde80235cf
zhengxiaoyao0716 <zhengxiaoyao0716@outlook.com>
2015-10-01 01:08:36 +0800

    加入Infomer回调接口，取代函数返回值，用以向游戏主体提交玩家的选择。 至此，非阻塞改造基本完成

--- DemoGame2048/src/DemoGame2048.java
+++ DemoGame2048/src/DemoGame2048.java
@@ -13,100 +13,52 @@ public class DemoGame2048 {
 
     public static void main(String[] args)
     {
-        Game2048 game2048 = null;
-        try {
-            game2048 = new Game2048(gameCommunicate, 4, 4, 2048);
-        } catch (IllegalArgumentException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
+        Game2048 game2048 = new Game2048(gameCommunicate, 4, 4, 2048);
 
         scanner = new Scanner(System.in);
-        GAME_LOOP:
         while (true)
         {
             if (isGameRunning)
             {
                 System.out.println("Game is Running.");
-                switch (scanner.next())
-                {
-                    case "w":
-                    case "up":
-                        try {
-                            game2048.action(Game2048.UP);
-                        } catch (IllegalArgumentException e) {
-                            // TODO Auto-generated catch block
-                            e.printStackTrace();
-                        }
-                        break;
-                    case "a":
-                    case "left":
-                        try {
-                            game2048.action(Game2048.LEFT);
-                        } catch (IllegalArgumentException e) {
-                            // TODO Auto-generated catch block
-                            e.printStackTrace();
-                        }
-                        break;
-                    case "d":
-                    case "right":
-                        try {
-                            game2048.action(Game2048.RIGHT);
-                        } catch (IllegalArgumentException e) {
-                            // TODO Auto-generated catch block
-                            e.printStackTrace();
-                        }
-                        break;
-                    case "s":
-                    case "down":
-                        try {
-                            game2048.action(Game2048.DOWN);
-                        } catch (IllegalArgumentException e) {
-                            // TODO Auto-generated catch block
-                            e.printStackTrace();
-                        }
-                        break;
 
-                    case "f":
-                    case "replay":
-                    {
-                        System.out.println("Keep level?");
-                        String input = scanner.next();
-                        game2048.replay(
-                                input.equals("g") || input.equals("yes"));
-                        System.out.println("Replay.");
-                    }break;
-                    case "g":
-                    case "quit":
-                    {
-                        if (game2048.quitGame())
-                        {
-                            System.out.println("Quit.");
-                            isGameRunning = false;
-                        }
-                        else System.out.println("Quit game failed");
-                    }break;
+                String s = scanner.next();
+                if (s.equals("w") || s.equals("up")) {
+                    game2048.action(Game2048.UP);
 
-                    case "h":
-                    case "backStep":
-                    {
-                        if (!game2048.backStep()) System.out.println("Back failed.");
-                        else System.out.println("backStep.");
-                    }break;
-                    case "j":
-                    case "cleanGrid":
-                    {
-                        System.out.println("Witch row?");
-                        int rowPos = scanner.nextInt();
-                        System.out.println("Witch col?");
-                        int colPos = scanner.nextInt();
-                        if (!game2048.cleanGrid(rowPos - 1, colPos - 1)) System.out.println("Clean failed.");
-                        else System.out.printf("The num in grid[%d, %d] has been cleaned.\n", rowPos, colPos);
-                    }break;
+                } else if (s.equals("a") || s.equals("left")) {
+                    game2048.action(Game2048.LEFT);
 
-                    default :
-                        System.out.println("Unknown input!");
-                        break;
+                } else if (s.equals("d") || s.equals("right")) {
+                    game2048.action(Game2048.RIGHT);
+
+                } else if (s.equals("s") || s.equals("down")) {
+                    game2048.action(Game2048.DOWN);
+
+                } else if (s.equals("f") || s.equals("replay")) {
+                    System.out.println("Keep level?");
+                    String input = scanner.next();
+                    game2048.replay(
+                            input.equals("g") || input.equals("yes"));
+                    System.out.println("Replay.");
+                } else if (s.equals("g") || s.equals("quit")) {
+                    if (game2048.finishGame()) {
+                        System.out.println("Quit.");
+                        isGameRunning = false;
+                    } else System.out.println("Quit game failed");
+                } else if (s.equals("h") || s.equals("backStep")) {
+                    if (!game2048.backStep()) System.out.println("Back failed.");
+                    else System.out.println("backStep.");
+                } else if (s.equals("j") || s.equals("cleanGrid")) {
+                    System.out.println("Witch row?");
+                    int rowPos = scanner.nextInt();
+                    System.out.println("Witch col?");
+                    int colPos = scanner.nextInt();
+                    if (!game2048.cleanGrid(rowPos - 1, colPos - 1)) System.out.println("Clean failed.");
+                    else System.out.printf("The num in grid[%d, %d] has been cleaned.\n", rowPos, colPos);
+                } else {
+                    System.out.println("Unknown input!");
+
                 }
             }
             else
@@ -114,31 +66,27 @@ public class DemoGame2048 {
                 System.out.println("Game is waiting.");
                 System.out.println("You can input 'f' or 'exit' to exit.");
                 System.out.println("Input 'g' or 'start' to start.");
-                switch (scanner.next())
-                {
-                    case "f":
-                    case "exit":
-                        System.out.println("Game end.");
-                        break GAME_LOOP;
-                    case "g":
-                    case "start":
-                    {
-                        game2048.startGame();
-                        isGameRunning = true;
-                        System.out.printf("Game start.\n"
-                                + "Commend list:\n"
-                                + "w->up\n"
-                                + "s->down\n"
-                                + "a->left\n"
-                                + "d->right\n"
-                                + "f->replay\n"
-                                + "g->quit\n"
-                                + "h->backStep\n"
-                                + "j->cleanGrid\n");
-                    }break;
-                    default :
-                        System.out.println("Unknown input!");
-                        break;
+
+                String s = scanner.next();
+                if (s.equals("f") || s.equals("exit")) {
+                    System.out.println("Game end.");
+                    break;
+                } else if (s.equals("g") || s.equals("start")) {
+                    game2048.startGame();
+                    isGameRunning = true;
+                    System.out.printf("Game start.\n"
+                            + "Commend list:\n"
+                            + "w->up\n"
+                            + "s->down\n"
+                            + "a->left\n"
+                            + "d->right\n"
+                            + "f->replay\n"
+                            + "g->quit\n"
+                            + "h->backStep\n"
+                            + "j->cleanGrid\n");
+                } else {
+                    System.out.println("Unknown input!");
+
                 }
             }
         }
@@ -149,21 +97,18 @@ public class DemoGame2048 {
     {
         @Override
         public Map<String, Object> loadData() {
-            // TODO Auto-generated method stub
             System.out.println("Loading data");
             return null;
         }
 
         @Override
         public boolean saveData(Map<String, Object> data) {
-            // TODO Auto-generated method stub
             System.out.println("Saving data");
             return false;
         }
 
         @Override
         public void showData(int level, int score, int[][] board) {
-            // TODO Auto-generated method stub
             System.out.printf("level:\t%d\n", level);
             System.out.printf("score:\t%d\n", score);
 
@@ -182,7 +127,7 @@ public class DemoGame2048 {
                 for (int grid : row)
                 {
                     if (grid!=0) System.out.printf("%4d\t", grid);
-                    else System.out.printf("    \t");
+                    else System.out.printf("%4c\t", ' ');
                 }
 
                 System.out.printf("||\n||");
@@ -198,59 +143,53 @@ public class DemoGame2048 {
         }
 
         @Override
-        public boolean levelUpIsEnterNextLevel(int level, int score) {
-            // TODO Auto-generated method stub
-            System.out.println("Level up!");
-            System.out.println("Are you willing to play hard-mode?");
-            String input = scanner.next();
-            return input.equals("g")||input.equals("yes");
+        public void noChangeRespond() {
+            System.out.println("Invalid action!");
         }
 
         @Override
-        public boolean gameOverIsReplay(int level, int score) {
-            // TODO Auto-generated method stub
-            System.out.println("Game over!");
-            System.out.println("Continue?");
-            String input = scanner.next();
-            if (input.equals("g")||input.equals("yes")) return true;
-            else
-            {
-                isGameRunning = false;
-                System.out.println("Quit.");
-                return false;
-            }
+        public void movedRespond() {
+            System.out.println("Moved.");
         }
 
         @Override
-        public boolean saveFailedIsStillQuit() {
-            // TODO Auto-generated method stub
+        public void mergedRespond() {
+            System.out.println("Merged.");
+        }
+
+        @Override
+        public void saveFailedIsStillQuit(Informer informer) {
             System.out.println("Save failed!");
             System.out.println("Still quit game?");
             String input = scanner.next();
             if (input.equals("g")||input.equals("yes"))
             {
                 isGameRunning = false;
-                return true;
+                informer.commit(true);
             }
-            else return false;
+            else informer.commit(false);
         }
 
         @Override
-        public void noChangeRespond() {
-            // TODO Auto-generated method stub
-            System.out.println("Invalid action!");
+        public void gameEndIsReplay(int level, int score, Informer informer) {
+            System.out.println("Game over!");
+            System.out.println("Continue?");
+            String input = scanner.next();
+            if (input.equals("g")||input.equals("yes")) informer.commit(true);
+            else
+            {
+                isGameRunning = false;
+                System.out.println("Quit.");
+                informer.commit(false);
+            }
         }
 
         @Override
-        public void movedRespond() {
-            // TODO Auto-generated method stub
-            System.out.println("Moved.");
-        }
-
-        @Override
-        public void mergedRespond() {
-            // TODO Auto-generated method stub
-            System.out.println("Merged.");
+        public void levelUpIsEnterNextLevel(int level, int score, Informer informer) {
+            System.out.println("Level up!");
+            System.out.println("Are you willing to play hard-mode?");
+            String input = scanner.next();
+            informer.commit(input.equals("g") || input.equals("yes"));
         }
     };
 }
